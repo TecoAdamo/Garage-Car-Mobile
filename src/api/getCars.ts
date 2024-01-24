2917f71163e40bd0e36440f013135bf36a60f2e8
Mateus Adamo <adamomateus478@gmail.com>
2024-01-24 19:32:08 -0300

    feat: Calling API

---
+++ src/api/getCars.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+import { CarModel } from "../cardView/props";
+
+import { CAR_API_BASE_URL } from "../constants/car";
+
+interface ApiResponse {
+  cars: CarModel[];
+}
+
+const fetchGetCarData = async (id: number) => {
+  try {
+    const response = await axios.get<ApiResponse>(CAR_API_BASE_URL);
+    const carData = response.data.cars.find((car) => car.id === id) || null;
+
+    return carData;
+  } catch (error) {
+    console.log("Erro ao buscar dados da API", error);
+  }
+};
